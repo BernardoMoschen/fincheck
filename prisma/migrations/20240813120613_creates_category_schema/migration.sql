d732424a77d586e9117cb8251b5b722f390be077
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-08-13 09:06:28 -0300

    feat:category model

---
+++ prisma/migrations/20240813120613_creates_category_schema/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "transaction_type" AS ENUM ('INCOME', 'EXPENSE');
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "name" TEXT NOT NULL,
+    "icon" TEXT NOT NULL,
+    "type" "transaction_type" NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- AddForeignKey
+ALTER TABLE "categories" ADD CONSTRAINT "categories_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
