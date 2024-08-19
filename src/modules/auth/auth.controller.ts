26cac93774bbfe4ba9732d421debb497a4600f55
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-08-19 15:19:26 -0300

    fix: signIn and signUp to public

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, SetMetadata } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/signInDto';
 import { SignupDto } from './dto/signUpDto';
@@ -8,11 +8,13 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
+  @SetMetadata('IS_PUBLIC', true)
   authenticate(@Body() authenticateDto: SignInDto) {
     return this.authService.signIn(authenticateDto);
   }
 
   @Post('sign-up')
+  @SetMetadata('IS_PUBLIC', true)
   create(@Body() signupDto: SignupDto) {
     return this.authService.signUp(signupDto);
   }
