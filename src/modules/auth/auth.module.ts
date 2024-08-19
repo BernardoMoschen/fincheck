2350347f7633fe9174c579e24845c961f064baff
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-08-19 10:01:32 -0300

    wip: jwt auth

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
+  imports: [
+    JwtModule.register({
+      signOptions: { expiresIn: '7d' },
+      secret: 'unsecure-jwt-secret',
+    }),
+  ],
 })
 export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthenticateDto } from './dto/authenticate.dto';
 import { UsersRepository } from 'src/shared/database/repositories/users.repositories';
+import { compare } from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly userRepo: UsersRepository) {}
+  constructor(
+    private readonly userRepo: UsersRepository,
+    private readonly jwtService: JwtService,
+  ) {}
 
   async authenticate(authenticateDto: AuthenticateDto) {
     const { email, password } = authenticateDto;
@@ -18,6 +23,15 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    return { user };
+    const isPasswordValid = await compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accessToken = await this.jwtService.signAsync({ sub: user.id });
+
+    return {
+      accessToken,
+    };
   }
 }
