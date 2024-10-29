8115c56b6da9e852370f0b92e3cf73de9d34c973
BernardoMoschen <bernardomoschen.dev@gmail.com>
2024-10-29 13:58:06 -0300

    feat:add delete transaction

--- src/modules/transactions/services/transactions.service.ts
+++ src/modules/transactions/services/transactions.service.ts
@@ -62,8 +62,18 @@ export class TransactionsService {
     });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} transaction`;
+  async remove(userId: string, transactionId: string) {
+    await this.validateTransactionOwnershipService.validate(
+      userId,
+      transactionId,
+    );
+    await this.transactionsRepo.delete({
+      where: {
+        id: transactionId,
+      },
+    });
+
+    return null;
   }
 
   private async validateEntitiesOwnership({
